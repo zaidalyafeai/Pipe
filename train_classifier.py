2f425449a53db5d876a78c1972bc4fa7343abca6
zaidalyafeai <alyafey22@gamil.com>
2025-07-17 00:04:40 +0300

    improve training classifier script

--- train_classifier.py
+++ train_classifier.py
@@ -5,16 +5,18 @@ from pytorch_lightning import LightningModule, Trainer
 from torch.utils.data import DataLoader
 from src.utils.embedder import BERTEmbeddings, JinaEmbeddingsV3TextMatching
 from src.utils.regression_head import RegressionHead
-from datasets import load_from_disk, load_dataset
+from datasets import load_from_disk, load_dataset, Dataset, DatasetDict
 import numpy as np
 import argparse
 from sklearn.metrics import f1_score
 from scipy.stats import spearmanr
 import multiprocessing
+import glob
+import json
 # multiprocessing.set_start_method('spawn', force=True)
 
 class BERTClassifier(LightningModule):
-    def __init__(self, dataset=None, input_column="input_text", target_column="score", base_model_name="UBC-NLP/ARBERT"):
+    def __init__(self, dataset=None, input_column="input_text", target_column="score", base_model_name="UBC-NLP/ARBERT", train_batch_size=256, eval_batch_size=128):
         super().__init__()
         if 'jina' in base_model_name.lower():
             self.embedder = JinaEmbeddingsV3TextMatching(device='cuda', model_id=base_model_name) 
@@ -25,7 +27,8 @@ class BERTClassifier(LightningModule):
         self.dataset = dataset
         self.input_column = input_column
         self.target_column = target_column
-
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
     def forward(self, input_ids, attention_mask, token_type_ids):
         with torch.no_grad(), torch.cuda.device('cuda'):      
             output = self.embedder.model(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
@@ -88,23 +91,15 @@ class BERTClassifier(LightningModule):
 
     def tokenize_dataset(self, examples):
         output = {}
-        tokenized = self.embedder.model.encode(
-                [example[self.input_column] for example in examples],
-                padding=True,
-                truncation=True,
-                max_length=512,
-                # return_tensors='pt',
-                task='text-matching')
-        output['input_ids'] = torch.tensor(tokenized, dtype=torch.long)
-        output['attention_mask'] = torch.ones_like(output['input_ids'], dtype=torch.long)
-        output['token_type_ids'] = torch.zeros_like(output['input_ids'], dtype=torch.long)
+        
+        output = self.embedder.tokenize(examples, self.input_column)
         output[self.target_column] = torch.tensor([example[self.target_column] for example in examples])
         return output
 
     def train_dataloader(self):
         return DataLoader(
             self.dataset['train'] if 'train' in self.dataset else self.dataset,
-            batch_size=256,
+            batch_size=self.train_batch_size,
             shuffle=True,
             collate_fn=self.tokenize_dataset,
             num_workers=0
@@ -116,6 +111,8 @@ class BERTClassifier(LightningModule):
             val_dataset = self.dataset['validation']
         elif 'val' in self.dataset:
             val_dataset = self.dataset['val']
+        elif 'test' in self.dataset:
+            val_dataset = self.dataset['test']
         else:
             # If no validation set, use 20% of training data
             train_dataset = self.dataset['train'] if 'train' in self.dataset else self.dataset
@@ -124,7 +121,7 @@ class BERTClassifier(LightningModule):
         
         return DataLoader(
             val_dataset,
-            batch_size=128,
+            batch_size=self.eval_batch_size,
             shuffle=False,
             collate_fn=self.tokenize_dataset,
             num_workers=0
@@ -133,7 +130,7 @@ class BERTClassifier(LightningModule):
     def test_dataloader(self):
         return DataLoader(
             self.test_dataset,
-            batch_size=128,
+            batch_size=self.eval_batch_size,
             shuffle=False,
             collate_fn=self.tokenize_dataset,
             num_workers=0
@@ -144,16 +141,51 @@ class BERTClassifier(LightningModule):
         checkpoint['hparams'] = {'input_dim': self.embedder.model.config.hidden_size, 'hidden_dim': 1024}
         return checkpoint
 
+def get_dataset(distilled_model_name):
+    examples = []
+    files = glob.glob(f"/ibex/ai/home/alyafez/.cache/jql/**/results.jsonl")
+    for file in files:
+        with open(file, "r") as f:
+            # get first line only
+            for i,line in enumerate(f):
+                data = json.loads(line)
+                if i == 0:
+                    if data['model'] != distilled_model_name:
+                        print(f"Skip: {data['model']}")
+                        break
+                    else:
+                        print(f"Found: {data['model']}")
+                examples.append({
+                    "input_text": data['input_text'],
+                    "score": int(data['score']),
+                })
+    if len(examples) == 0:
+        raise ValueError(f"No dataset found for {distilled_model_name}")
+
+    dataset = Dataset.from_list(examples)
+    # split the dataset into train and test
+    train_size = int(0.9 * len(dataset))
+    train_dataset = dataset.select(range(train_size))
+    test_dataset = dataset.select(range(train_size, len(dataset)))
+    dataset = DatasetDict({
+        'train': train_dataset,
+        'test': test_dataset
+    })
+    return dataset
+
 def main(args):
-    dataset = load_from_disk(args.dataset_name)
+    dataset = get_dataset(args.distilled_model_name)
     dataset = dataset.map(lambda x: {args.target_column: np.clip(int(x[args.target_column]), 0, 5)}, num_proc=4)
     dataset = dataset.filter(lambda x: x[args.target_column] != 5)
     
+    print(dataset)
     model = BERTClassifier(
         dataset=dataset, 
         input_column=args.input_column, 
         target_column=args.target_column,
-        base_model_name=args.base_model_name
+        base_model_name=args.base_model_name,
+        train_batch_size=args.train_batch_size,
+        eval_batch_size=args.eval_batch_size
     )
 
     # gpu type detection
@@ -167,9 +199,10 @@ def main(args):
         precision = "32"
 
     print(f"Using precision: {precision}")
+    num_steps = len(dataset['train']) // args.train_batch_size
     trainer = Trainer(
         max_epochs=1,
-        val_check_interval=500,
+        val_check_interval=num_steps // 4,
         precision=precision,
         num_sanity_val_steps=0    
     )
@@ -177,22 +210,22 @@ def main(args):
     trainer.save_checkpoint(f"{args.checkpoint_dir}/regression_head.ckpt")    
     
 if __name__ == "__main__":
-    base_path = '/ibex/ai/home/alyafez/curator/examples/custom'
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--base_model_name", type=str, default="UBC-NLP/ARBERT"
     )
     parser.add_argument(
-        "--dataset_name",
+        "--distilled_model_name",
         type=str,
-        default=f"{base_path}/annotated_dataset",
+        default=f"gemma-3-27b-it",
     )
-    
     parser.add_argument("--input_column", type=str, default="input_text")
     parser.add_argument("--target_column", type=str, default="score")
+    parser.add_argument("--train_batch_size", type=int, default=256)
+    parser.add_argument("--eval_batch_size", type=int, default=128)
     parser.add_argument("--eval_only", action="store_true")
     args = parser.parse_args()
-    args.checkpoint_dir = args.base_model_name.replace('/', '__')
+    args.checkpoint_dir = args.base_model_name.replace('/', '__')+'@'+args.distilled_model_name.replace('/', '__')
     print(args)
 
     main(args)
