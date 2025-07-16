399e31669cd90aba56627ad571741bc7ac7e89ee
zaidalyafeai <alyafey22@gamil.com>
2025-07-17 00:06:59 +0300

    use variables in sbatch

--- script.sh
+++ script.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
 HOST=localhost
 PORT=8787
-MODEL_NAME=gemma-3-4b-it
-MODEL_PATH=${MODEL_NAME}
 MAX_TOKENS=4096
 LANGUAGE=arb_Arab
+MODEL_NAME=$1
+GPU_COUNT=$2
+WORK_DIR="/ibex/ai/home/alyafez/JQL-Annotation-Pipeline"
+VENV_PATH="/ibex/ai/home/alyafez/.vllm/bin/activate"
+cd ${WORK_DIR}
+source ${VENV_PATH}
 CMD="
     vllm serve \
-        ${MODEL_PATH} \
+        ${MODEL_NAME} \
         --host ${HOST} \
         --port ${PORT} \
         --gpu-memory-utilization 0.95 \
-        --tensor-parallel-size $1 \
+        --tensor-parallel-size ${GPU_COUNT} \
         --pipeline-parallel-size 1 \
         --max-model-len ${MAX_TOKENS} \
         --max-num-seqs 100 \
