ddab64e0ee347bd0df032e56e7329460cfeefab1
zaidalyafeai <alyafey22@gamil.com>
2025-08-12 13:13:46 +0300

    add example to load compression parquet

--- test_loading.py
+++ test_loading.py
@@ -1,27 +1,20 @@
 from datasets import load_dataset
-import os
-import pyarrow.parquet as pq
+from run_compression import get_total_examples
 from glob import glob
-base_path = "/ibex/ai/project/c2254/arabic_filtered_data/fineweb2_arabic_threshold_2/data/arb_Arab/train/compressed/"
-def get_total_examples(base_path):
-    total_examples = 0
-    files = sorted(glob(base_path + "*.parquet"))
-    print(files)
-    for file in files:
-        metadata = pq.read_metadata(file)
-        num_examples = metadata.num_rows
-        print(file.split("/")[-1], num_examples)
-        total_examples += num_examples
-    return total_examples   
+base_path = "/ibex/ai/project/c2254/arabic_filtered_data/fineweb2_arabic_threshold_2/data/arb_Arab/train/"
+compressed_path = base_path + "compressed/"
 
 def main():
-    total_examples = get_total_examples(base_path)
-    print(total_examples)
-    # all_files = glob("/ibex/ai/project/c2254/arabic_filtered_data/fineweb2_arabic_threshold_2/data/arb_Arab/train/compressed/*.parquet")
-    # print(sorted(all_files))
-    # for i in range(0, 64):
-    #     fw = load_dataset("parquet", data_files=[base_path + f"{i:05d}.parquet"], split = 'train', num_proc=1)
-    #     print(fw)
+    files, total_examples = get_total_examples(base_path)
+    compressed_files, total_compressed_examples = get_total_examples(compressed_path)
+    print('len of files', len(files))
+    print('len of compressed files', len(compressed_files))
+    print(total_examples, total_compressed_examples)
+    print()    
+    fw = load_dataset("parquet", data_files=[files[-1]], split = 'train', num_proc=1)
+    print(fw[-1])
+    fw = load_dataset("parquet", data_files=[compressed_files[-1]], split = 'train', num_proc=1)
+    print(fw[-1])
 
 if __name__ == "__main__":
     main()
