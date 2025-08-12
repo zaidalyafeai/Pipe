0b489160eb42747cbc252efe2c088cfb40fdbf1a
zaidalyafeai <alyafey22@gamil.com>
2025-08-12 12:14:29 +0300

    update slurm script for compression

--- run_compression.py
+++ run_compression.py
@@ -1,81 +1,37 @@
 import argparse
 from datasets.utils.logging import disable_progress_bar
-disable_progress_bar()
+# disable_progress_bar()
 from datasets import load_dataset, concatenate_datasets
 import pyarrow.parquet as pq
 import os
 from glob import glob
 
-def get_total_examples(args):
+def get_total_examples(base_path):
     total_examples = 0
-    files = sorted(glob(args.base_path + "*.parquet"))
+    files = sorted(glob(base_path + "*.parquet"))
     for file in files:
         metadata = pq.read_metadata(file)
         num_examples = metadata.num_rows
-        # print(file, num_examples)
         total_examples += num_examples
     return files, total_examples
-
-def get_total_examples_dummy():
-    return sum([len(x) for x in dummy_examples])   
-        
-dummy_examples = [[11, 12, 13, 14], [21, 22, 23, 24], [31, 32, 33, 34]]
-
-def get_shard_examples(shard, num_shards):
-    files, total_examples = get_total_examples(args)
-    max_examples = total_examples // num_shards
-    # files = dummy_examples
-    curr_total_examples = 0
-    fw = None
-    for i in range(0, len(files)):
-        first_file_num_rows = pq.read_metadata(files[i]).num_rows
-        # first_file_num_rows = len(files[i])
-        curr_range = (curr_total_examples, curr_total_examples + first_file_num_rows)
-        if shard * max_examples >= curr_range[0] and shard * max_examples < curr_range[1]:
-            print(i, first_file_num_rows)
-            fw1 = load_dataset("parquet", data_files=[files[i]], split = 'train', num_proc=1)
-            start_idex = shard * max_examples % (first_file_num_rows)
-            end_idex = min(start_idex + max_examples, len(fw1))
-            print('start', start_idex)
-            print('end', end_idex)
-            fw1 = fw1.select(range(start_idex, end_idex))
-            fw = fw1
-            if len(fw1) < max_examples:
-                print('concatenating')
-                # print('fw1', len(fw1))
-                if i + 1 < len(files):
-                    print('adding ', max_examples - len(fw1))
-                    fw2 = load_dataset("parquet", data_files=[files[i + 1]], split = 'train', num_proc=1).select(range(0, max_examples - len(fw1)))
-                    # print('fw2', len(fw2))
-                    fw = concatenate_datasets([fw1, fw2])
-            assert len(fw) == max_examples, f"fw length {len(fw)} != max_examples {max_examples}"
-            if i == len(files) - 1 and shard == num_shards - 1: # flush last shard
-                # print('flushing')
-                _fw = load_dataset("parquet", data_files=[files[i]], split = 'train', num_proc=1)
-                fw = concatenate_datasets([fw, _fw.select(range(end_idex, len(_fw)))])
-        curr_total_examples += first_file_num_rows
-    return fw
 
 def get_file_idex(files, shard, max_examples):
     curr_total_examples = 0
     for i in range(0, len(files)):
         first_file_num_rows = pq.read_metadata(files[i]).num_rows
-        # first_file_num_rows = len(files[i])
         curr_range = (curr_total_examples, curr_total_examples + first_file_num_rows)
         if shard * max_examples >= curr_range[0] and shard * max_examples < curr_range[1]:
             return curr_total_examples, i
         curr_total_examples += first_file_num_rows
     return None
         
-def get_shard_examplesv2(shard, num_shards):
-    files, total_examples = get_total_examples(args)
-    max_examples = total_examples // num_shards
-    # files = dummy_examples
-    fw = None
-    total_before, i = get_file_idex(files, shard, max_examples)
+def get_shard_examplesv2(args):
+    files, total_examples = get_total_examples(args.base_path)
+    max_examples = total_examples // args.num_shards
+    total_before, i = get_file_idex(files, args.shard, max_examples)
     num_rows = pq.read_metadata(files[i]).num_rows
     fw1 = load_dataset("parquet", data_files=[files[i]], split = 'train', num_proc=1)
-    start_idex = shard * max_examples - total_before
+    start_idex = args.shard * max_examples - total_before
     end_idex = min(start_idex + max_examples, num_rows)
     print(i, start_idex, end_idex)
     fw1 = fw1.select(range(start_idex, end_idex))
@@ -85,19 +41,13 @@ def get_shard_examplesv2(shard, num_shards):
             fw2 = load_dataset("parquet", data_files=[files[i + 1]], split = 'train', num_proc=1).select(range(0, max_examples - len(fw1)))
             fw = concatenate_datasets([fw1, fw2])
     assert len(fw) == max_examples, f"fw length {len(fw)} != max_examples {max_examples}"
-    if i == len(files) - 1 and shard == num_shards - 1: # flush last shard
+    if i == len(files) - 1 and args.shard == args.num_shards - 1: # flush last shard
         _fw = load_dataset("parquet", data_files=[files[i]], split = 'train', num_proc=1)
         fw = concatenate_datasets([fw, _fw.select(range(end_idex, len(_fw)))])
     return fw
+
 def main(args):
-    # total_after_split = 0
-    # for i in range(0, args.num_shards):
-    #     print(f"Processing shard {i}")
-    #     fw = get_shard_examplesv2(i, args.num_shards)
-    #     # print(len(fw))
-    #     total_after_split += len(fw)
-    # print(total_after_split)
-    fw = get_shard_examplesv2(args.shard, args.num_shards)
+    fw = get_shard_examplesv2(args)
     print(len(fw))
 
     while True:
